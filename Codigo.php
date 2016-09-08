94c3f10aa0455e2c1e42bf387a49fd37fa0cd65b
KarenAlvarez01 <karenalvarez657@gmail.com>
2016-09-08 15:43:04 -0600

    practica de php

---
+++ Codigo.php
@@ -0,0 +1,10 @@
+<?PHP
+
+$n=0;
+while ($n<50) {
+	$n=rand(1,100);
+	echo "$n";
+
+}
+echo "Ahora el numero mayor que 50 numero"-$n;
+?>
---
+++ promedio3.5.php
@@ -0,0 +1,26 @@
+<?php
+$est =1;
+$prom=0;
+while ($est<=5){
+	$nota1=rand(0,5);
+	$nota2=rand(0,5);
+	$nota3=rand(0,5);
+	$nota4=rand(0,5);
+	echo "Estas son las notas de los estudiantes $est";
+	echo "La primera nota es $nota1 <br>";
+	echo "La primera nota es $nota2 <br>";
+	echo "La primera nota es $nota3 <br>";
+	echo "La primera nota es $nota4 <br>";
+
+	$prom = ($nota1+$nota2+$nota3+$nota4)/4;
+	if ($prom == 3.5)
+	{
+		echo "Usted a ganado $prom <br>";
+	}elseif ($prom<3.5)
+	 {
+		echo "Usted perdio $prom <br> ";
+	}
+	$est++;
+}
+
+?>
